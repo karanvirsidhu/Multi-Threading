a3d72b9cf3b886d47645a8cab0c878bbdbd2321a
Karanvir <iamkaranvir93@gmail.com>
2019-01-24 17:56:11 -0500

    Synchronized keyword example

---
+++ MutltiThreading/src/synchronizedDemo/App.java
@@ -0,0 +1,51 @@
+package synchronizedDemo;
+
+public class App {
+
+    private int count =0;
+
+    // every object has intrinsic lock, for using synchronize method firstly intrinsic lock is acquired
+    public synchronized void increment(){
+        count++;
+    }
+
+    public static void main(String[] args){
+
+        App app = new App();
+        app.doWork();
+    }
+
+    public void doWork() {
+
+        Thread t1= new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0;i<10000;i++){
+                    increment();
+                }
+            }
+        });
+
+        Thread t2= new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0;i<10000;i++){
+                    increment();
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Count is "+count);
+
+    }
+}
